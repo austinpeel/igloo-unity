ff10866ed7e1822362594d8208e5c1f96e1586ab
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2023-04-01 12:59:01 +0200

    Updated gitignore and AxisUIEditor

--- Assets/LensPlane/Scripts/Lines UI/Editors/AxisUIEditor.cs
+++ Assets/LensPlane/Scripts/Lines UI/Editors/AxisUIEditor.cs
@@ -4,14 +4,19 @@ using UnityEditor;
 
 [CustomEditor(typeof(AxisUI))]
 [CanEditMultipleObjects]
-public class AxisUIEditor : LineUIEditor
+public class AxisUIEditor : Editor
 {
+    private SerializedProperty width;
+    private SerializedProperty lineColor;
     private SerializedProperty labelAxis;
     private SerializedProperty isAxisX;
     private AxisUI axisUI;
+    private LineUI lineUI;
 
     private void OnEnable() 
     {
+        width = serializedObject.FindProperty("width");
+        lineColor = serializedObject.FindProperty("lineColor");
         labelAxis = serializedObject.FindProperty("labelAxis");
         isAxisX = serializedObject.FindProperty("isAxisX");
     }
@@ -19,12 +24,25 @@ public class AxisUIEditor : LineUIEditor
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
+        EditorGUILayout.PropertyField(width);
+        EditorGUILayout.PropertyField(lineColor);
         EditorGUILayout.PropertyField(labelAxis);
         EditorGUILayout.PropertyField(isAxisX);
 
-        axisUI = (AxisUI) target;
+        lineUI = (LineUI) target;
+        
+        if (!lineUI.GetWidth().Equals(width.floatValue))
+        {
+            lineUI.SetWidth(width.floatValue, true);
+        }
 
-        // Check if a value has changed and update accordingly
+        if (!lineUI.GetColor().Equals(lineColor.colorValue))
+        {
+            lineUI.SetColor(lineColor.colorValue, true);
+        }
+
+         axisUI = (AxisUI) target;
+
         if (!axisUI.GetLabelAxis().Equals((Image) labelAxis.objectReferenceValue))
         {
             axisUI.SetLabelAxis((Image) labelAxis.objectReferenceValue, true);
