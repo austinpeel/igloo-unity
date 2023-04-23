fb7f6d25aaedbdfa2c275aacb8511860ebebdee3
shoutizix <shoutizix@gmail.com>
2023-04-24 00:22:30 +0200

    Added beginning of Sersic function

--- Assets/Utils/Scripts/Profiles.cs
+++ Assets/Utils/Scripts/Profiles.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public static class LensProfiles
 {
+    // MASS PROFILES
+
     // Compute the convergence Kappa of the SIE profile
     // x and y coordinates and Einstein Radius in arcsec
     // Angle in degree
@@ -27,5 +29,29 @@ public static class LensProfiles
         // From COOLEST :
         // Kappa = einsteinRadius / (2 * Mathf.sqrt((x^2) + (y^2)))
         return einsteinRadius / (2f * Mathf.Sqrt((x*x) + (y*y)));
+    }
+
+
+    // LIGHT PROFILES
+
+    // Compute the brightness of the Sérsic profile
+    // x and y coordinates and Einstein Radius in arcsec
+    // Angle in degree
+    public static float BrightnessSersic(float x, float y, float sersicIndex, float halfLightRadius, float q, float angle)
+    {
+        // From COOLEST :
+        // With the major axis of the ellipsoid along the x axis
+        // b_n = 1.9992*sersicIndex - 0.3271
+        // Brightness = I_eff * exp(-b_n * ((Mathf.sqrt(q * (x^2) + (y^2) / q) / halfLightRadius) ^ (1/sersicIndex) - 1))
+        float angleInRad = (90f + angle) * Mathf.Deg2Rad;
+
+        float rotatedX = x * Mathf.Cos(angleInRad) + y * Mathf.Sin(angleInRad);
+        float rotatedY = -x * Mathf.Sin(angleInRad) + y * Mathf.Cos(angleInRad);
+
+        if (halfLightRadius == 0f) return 0f;
+
+        // TODO : Implement Sersic
+
+        return -1f;
     }
 }
